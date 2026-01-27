bee8e67809d302cea65de5bf502f4b4bf39aab01
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-27 20:39:58 +0530

    feat: configure React Router with shared layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import Menubar from './components/Menubar.jsx';
 import Home from "./pages/Home.jsx";
 import Footer from "./components/Footer.jsx";
+import {Route, Routes} from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Menubar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </div>
   )
--- client/src/components/Menubar.jsx
+++ client/src/components/Menubar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import {assets} from "../assets/assets.js";
 import {Menu, X} from "lucide-react";
+import {Link, useNavigate} from "react-router-dom";
 
 const Menubar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -8,12 +9,12 @@ const Menubar = () => {
   return (
     <nav className="bg-white px-8 py-4 flex justify-between items-center">
         {/* Left side: logo + text*/}
-        <div className="flex item-center space-x-2">
+        <Link className="flex item-center space-x-2" to="/">
             <img src={assets.logo} alt="logo" className="h-8 w-8 object-contain cursor-pointer"/>
             <span className="text-2xl font-semibold text-indigo-700 cursor-pointer">
                 remove.<span className="text-gray-400 cursor-pointer">bg</span>
             </span>
-        </div>
+        </Link>
         {/* Right side: Action buttons*/}
         <div className="hidden md:flex items-center space-x-4">
             <button className="text-gray-700 hover:text-blue-500 font-medium">
