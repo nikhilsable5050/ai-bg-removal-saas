4cff17d8c1f3d01b3d315ecc931f369ef3ac2d13
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-27 20:50:01 +0530

    feat: integrate react-hot-toast notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import Menubar from './components/Menubar.jsx';
 import Home from "./pages/Home.jsx";
 import Footer from "./components/Footer.jsx";
 import {Route, Routes} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
       <Menubar />
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
