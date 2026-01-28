82ad6d5d5fe6553a1ada5cef460325f39e1db3fb
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-28 21:26:11 +0530

    feat: add removebg backend module

---
+++ removebg/src/test/java/com/nikhilsable/removebg/RemovebgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.removebg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemovebgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
